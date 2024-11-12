fc812e175e7b3ac4e9862ae78a7c290443e174f1
devfardin <contactfardin22@gmail.com>
2024-11-13 03:53:32 +0600

    make theme all files and footer

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php 
+define( 'BAITU_THEME_DIR', __DIR__ );
+define( 'BAITU_THEME_URL', get_stylesheet_directory_uri() );
+
+function baitu_theme_enqueue_style(){
+	wp_enqueue_style(
+		'cf-style',
+		BAITU_THEME_URL . '/assets/css/main.css',
+		array(),
+		filemtime( BAITU_THEME_DIR . '/assets/css/main.css' )
+	);
+}
+add_action("wp_enqueue_scripts", "baitu_theme_enqueue_style");
